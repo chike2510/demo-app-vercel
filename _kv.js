2c67fcbaa12564d524a7a3b8f31642e3c7798cc5
chike2510 <onovochikeziri84@gmail.com>
2026-06-30 19:17:09 +0100

    Delete _kv.js

--- _kv.js
+++
@@ -1,7 +0,0 @@
-// Centralized Redis client using the current supported package.
-// Works with Upstash Redis connected via Vercel Marketplace.
-const { Redis } = require('@upstash/redis');
-
-const kv = Redis.fromEnv();
-
-module.exports = { kv };
