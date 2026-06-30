9e245200a4e1b9db5ffc6c5d4f722de720b3609a
chike2510 <onovochikeziri84@gmail.com>
2026-06-30 06:11:43 +0100

    Delete api/ai directory

--- api/ai/usage.js
+++
@@ -1,9 +0,0 @@
-const { verifyToken } = require('../../_auth');
-const { getUsageStats } = require('../../_db');
-
-module.exports = async (req, res) => {
-  const decoded = verifyToken(req);
-  if (!decoded) return res.status(401).json({ error: 'No valid token provided. Please log in.' });
-
-  res.json(getUsageStats(decoded.userId));
-};
